a36432295666e7d861b915c89e86352262205a96
JAYANTHA-W10\Jayanth Acharya <jayanth.acharya@robosofin.com>
2017-05-16 12:20:40 +0530

    - initial commit

---
+++ bot.js
@@ -0,0 +1,26 @@
+var Twit = require('twit');
+var config = require('./config');
+
+var http = require('http');
+var T = new Twit(config);
+
+var request = require("request");
+
+setInterval(tellajoke, 3600000);
+tellajoke();
+function tellajoke() {
+    request("https://api.chucknorris.io/jokes/random", function (error, response, body) {
+        var obj = JSON.parse(body)
+        console.log(obj.value);
+        tweetIt(obj.value);
+    });
+}
+
+function tweetIt(text) {
+
+    T.post('statuses/update', {
+        status: text
+    }, function (err, data, response) {
+        console.log(data)
+    });
+}
---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+     consumer_key:         'qPOytO4YWcmMz6yTWRnPAmqck',
+  consumer_secret:      'd2cmYBJvmGUKBe5gVpOxxbAVdF5gz44abqMIarp7a3DeCYzsUE',
+  access_token:         '864113234579607552-KREXmyHBKM2WpHcOd8Fm6S77Emx8eYz',
+  access_token_secret:  'n3p4IGjfuSwFHxCV7aUND27K5VlGTkktmMNd8rDfAKS7E',
+}
